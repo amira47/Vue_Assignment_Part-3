cbe9ce847133e6c02333254f748a07a947f5d7f8
Amira Hossain <amiravery@gmail.com>
2020-07-17 17:35:01 +0600

    initial

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+<html>
+  <div class="hello">
+    <b-table striped hover :items="items"></b-table>
+  </div>
+</html>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          Name: "Rohan",
+          Score: 10,
+          Duration: 30
+        }
+      ]
+    };
+  }
+};
+</script>
+
+
+<style scoped>
+th {
+  background-color: teal;
+}
+</style>
